a1e7ba3a2f6606ab541e9ce8b243b148ca29e105
Unknown <adamsha.asif@gmail.com>
2017-10-04 20:58:14 +0200

    edits

--- ClientAndroid/app/src/main/java/com/esgi/cityfinder/MainActivity.java
+++ ClientAndroid/app/src/main/java/com/esgi/cityfinder/MainActivity.java
@@ -1,19 +1,26 @@
 package com.esgi.cityfinder;
 
+import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.hololo.tutorial.library.Step;
 import com.hololo.tutorial.library.TutorialActivity;
 
 public class MainActivity extends TutorialActivity implements View.OnClickListener{
 
+    Button buttonFinish;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
+        buttonFinish = (Button)findViewById(R.id.next);
+
         addFragment(new Step.Builder().setTitle("This is header")
                 .setContent("This is content")
                 .setDrawable(R.drawable.eiffle_tower)
@@ -27,6 +34,14 @@ public class MainActivity extends TutorialActivity implements View.OnClickListen
                 .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
                 .setSummary("This is summary")
                 .build());
+
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Finished",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this,SearchActivity.class));
+            }
+        });
 
     }
 }
