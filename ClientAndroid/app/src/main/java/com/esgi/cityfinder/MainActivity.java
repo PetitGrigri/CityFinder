35873e937e0db2f991889013846efcb751368c24
Unknown <adamsha.asif@gmail.com>
2017-10-04 20:23:50 +0200

    Welcome screen test

--- ClientAndroid/app/src/main/java/com/esgi/cityfinder/MainActivity.java
+++ ClientAndroid/app/src/main/java/com/esgi/cityfinder/MainActivity.java
@@ -1,13 +1,32 @@
 package com.esgi.cityfinder;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+public class MainActivity extends TutorialActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setDrawable(R.drawable.eiffle_tower)
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setSummary("This is summary")
+                .build());
+
+        addFragment(new Step.Builder().setTitle("This is header 2")
+                .setContent("This is content")
+                .setDrawable(R.drawable.geode)
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setSummary("This is summary")
+                .build());
+
     }
 }
